5a294f4446f8696e1cf7890d141fd255e6f163f2
JodliDev <jodlidev@gmail.com>
2017-05-06 15:08:36 +0200

    Create repository

---
+++ README.md
@@ -0,0 +1,24 @@
+# DsMMO
+
+##Full Moon
+All abilities have a 50 percent better chance to be successfull during a full moon
+
+
+## Abilities
+Evertime an ability
+
+
+### CHOP
+### MINE
+### DIG
+### REPAIR
+### ATTACKED
+### ATTACK
+### PLANT
+### FERTILIZE
+### EAT
+
+
+## Magic
+
+### Secret of moles
---
+++ modinfo.lua
@@ -0,0 +1,20 @@
+name = "DSMMO"
+
+description = "Inspired by MCMMO for mincraft. This mod adds a levelup system for certain actions. The more often you do an action, the more perks you will receive from it"
+
+author = "Jodli Developement"
+
+version = "1.0"
+
+api_version = 10
+
+dont_starve_compatible = false
+reign_of_giants_compatible = true
+dst_compatible = true
+
+forumthread = ""
+
+all_clients_require_mod = true
+clients_only_mod = false
+
+server_filter_tags = {"Character", "Utility"}
